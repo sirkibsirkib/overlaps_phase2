6dac56f25c98d31a8eb04d35ee64871bdbe3e2b0
sirkibsirkib <christopher.esterhuyse@gmail.com>
2017-06-14 09:57:58 +0200

    good stuff

--- scriptQuick.py
+++ scriptQuick.py
@@ -6,8 +6,12 @@ out_dir = './outputs/'
 e = 0.012
 t = 80
 
+modes = {"kucherov_1","kucherov_2","kucherov_3","kucherov_4","kucherov_5","kucherov_6","valimaki2"}
+fasta_path = "5VM_real.p1.40000.fasta"
+
+
 fasta_paths = [f for f in os.listdir(in_dir) if f.endswith('.fasta')]
-for fasta_path in fasta_paths:
+for mode in modes:
 	out_path = out_dir + fasta_path[:-6] + '.out'
 	if os.path.isfile(out_path):
 		print(out_path, "already exists. skipping.")
@@ -15,10 +19,10 @@ for fasta_path in fasta_paths:
 		print(out_path, "solving...")
 		call_str = ['./rust_overlaps_measure.exe',
 					in_dir+fasta_path,
-					out_path,
+					out_path[:-4]+mode+'.out',
 					str(e),
 					str(t),
-					'-r', '-t', '-m=kucherov_1'
+					'-r', '-t', ('-m='+mode)
 					]
 		print('CALLING', ' '.join(call_str))
 		subprocess.call(call_str)
